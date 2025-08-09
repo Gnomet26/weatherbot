8cf722bccc65ccc9f0a2879b03a0f071e9996d70
Gnomet26 <Gnometerminal026@gmail.com>
2025-08-09 16:29:15 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,9 +10,13 @@ bot = Flask(__name__)
 
 
 @bot.route(f"/{BOT_TOKEN}", methods = ["POST"])
-def bot_update():
+def bot_updates():
     update = request.get_json()
+    if not update:
+        return {"ok": False}, 400
     filter_message(update)
+    return {"ok": True}, 200
+
 
 
 @bot.route("/health", methods=["GET"])

5769812ac21683e2129f770becefd0440754b2cc
Gnomet26 <Gnometerminal026@gmail.com>
2025-08-09 11:08:23 +0500

    Add files via upload

---
+++ psql/secret.py
@@ -0,0 +1,6 @@
+
+DB_USER = 'bot_users_37jy_user'
+DB_PASSWORD = 'LeZ5MLt1MG50gxGJwL3U6fJq9diic85x'
+DB_HOST = 'dpg-d2bdtfur433s739mp820-a.singapore-postgres.render.com'
+DB_PORT = '5432'
+DB_NAME = 'bot_users_37jy'

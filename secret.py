18dac53a6a74f32c238cc427a8518b8d255de8da
Gnomet26 <Gnometerminal026@gmail.com>
2025-08-09 11:07:51 +0500

    Add files via upload

---
+++ secret.py
@@ -0,0 +1,6 @@
+BOT_TOKEN = "8037107793:AAEFgXJYEPHxMJWHpBIQU_1gzGdh29wt4AE"
+BOT_BASE_URL = f"https://api.telegram.org/bot{BOT_TOKEN}"
+NGROK_TOKEN = "30jjbfHM2SysBrOvaxZLVymWLw3_719rHqSK3jtkvuLbbUv8i"
+BASE_PORT = 5000
+
+BACKENT_BASE_URL = "https://weatherapi-xdnd.onrender.com/api"
